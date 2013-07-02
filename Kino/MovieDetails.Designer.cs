7ba98a30b7cf9273921a03ee7d03e0d8c8d71a46
Andrej Gasteovski <andrej.gasteovski@gmail.com>
2013-07-02 19:32:41 +0200

    Ratings implemented

--- Kino/MovieDetails.Designer.cs
+++ Kino/MovieDetails.Designer.cs
@@ -43,10 +43,29 @@
             this.tbGenre = new System.Windows.Forms.TextBox();
             this.tbDuration = new System.Windows.Forms.TextBox();
             this.tbRating = new System.Windows.Forms.TextBox();
-            this.tbWriter = new System.Windows.Forms.TextBox();
             this.lbMovieName = new System.Windows.Forms.Label();
-            this.btnAddRating = new System.Windows.Forms.Button();
-            this.tbAddRating = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.tbWriter = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -179,14 +198,6 @@
             this.tbRating.Size = new System.Drawing.Size(111, 20);
             this.tbRating.TabIndex = 14;
             // 
-            // tbWriter
-            // 
-            this.tbWriter.Location = new System.Drawing.Point(115, 226);
-            this.tbWriter.Name = "tbWriter";
-            this.tbWriter.ReadOnly = true;
-            this.tbWriter.Size = new System.Drawing.Size(111, 20);
-            this.tbWriter.TabIndex = 15;
-            // 
             // lbMovieName
             // 
             this.lbMovieName.AutoSize = true;
@@ -197,30 +208,249 @@
             this.lbMovieName.TabIndex = 16;
             this.lbMovieName.Text = "label9";
             // 
-            // btnAddRating
+            // radioButton1
             // 
-            this.btnAddRating.Location = new System.Drawing.Point(58, 261);
-            this.btnAddRating.Name = "btnAddRating";
-            this.btnAddRating.Size = new System.Drawing.Size(75, 21);
-            this.btnAddRating.TabIndex = 17;
-            this.btnAddRating.Text = "Add rating";
-            this.btnAddRating.UseVisualStyleBackColor = true;
-            this.btnAddRating.Click += new System.EventHandler(this.btnAddRating_Click);
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(60, 302);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // tbAddRating
+            // label9
             // 
-            this.tbAddRating.Location = new System.Drawing.Point(151, 261);
-            this.tbAddRating.Name = "tbAddRating";
-            this.tbAddRating.Size = new System.Drawing.Size(75, 20);
-            this.tbAddRating.TabIndex = 18;
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(55, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Rate this movie:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 302);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(100, 302);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 22;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(120, 302);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 23;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(140, 302);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(14, 13);
+            this.radioButton5.TabIndex = 24;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(160, 302);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(14, 13);
+            this.radioButton6.TabIndex = 25;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(180, 302);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(14, 13);
+            this.radioButton7.TabIndex = 26;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(200, 302);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(14, 13);
+            this.radioButton8.TabIndex = 27;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(220, 302);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(14, 13);
+            this.radioButton9.TabIndex = 28;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(240, 302);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(14, 13);
+            this.radioButton10.TabIndex = 29;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(61, 318);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(80, 318);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "2";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(99, 318);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "3";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(120, 318);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(13, 13);
+            this.label13.TabIndex = 33;
+            this.label13.Text = "4";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(140, 318);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(13, 13);
+            this.label14.TabIndex = 34;
+            this.label14.Text = "5";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(159, 318);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(13, 13);
+            this.label15.TabIndex = 35;
+            this.label15.Text = "6";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(178, 318);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(13, 13);
+            this.label16.TabIndex = 36;
+            this.label16.Text = "7";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(200, 318);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(13, 13);
+            this.label17.TabIndex = 37;
+            this.label17.Text = "8";
+            // 
+            // tbWriter
+            // 
+            this.tbWriter.Location = new System.Drawing.Point(115, 226);
+            this.tbWriter.Name = "tbWriter";
+            this.tbWriter.ReadOnly = true;
+            this.tbWriter.Size = new System.Drawing.Size(111, 20);
+            this.tbWriter.TabIndex = 15;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(219, 318);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(13, 13);
+            this.label18.TabIndex = 38;
+            this.label18.Text = "9";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(238, 318);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(19, 13);
+            this.label19.TabIndex = 39;
+            this.label19.Text = "10";
             // 
             // MovieDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 301);
-            this.Controls.Add(this.tbAddRating);
-            this.Controls.Add(this.btnAddRating);
+            this.ClientSize = new System.Drawing.Size(292, 379);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.lbMovieName);
             this.Controls.Add(this.tbWriter);
             this.Controls.Add(this.tbRating);
@@ -261,10 +491,29 @@
         private System.Windows.Forms.TextBox tbGenre;
         private System.Windows.Forms.TextBox tbDuration;
         private System.Windows.Forms.TextBox tbRating;
-        private System.Windows.Forms.TextBox tbWriter;
         private System.Windows.Forms.TextBox tbYear;
         private System.Windows.Forms.Label lbMovieName;
-        private System.Windows.Forms.Button btnAddRating;
-        private System.Windows.Forms.TextBox tbAddRating;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox tbWriter;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
     }
 }
--- Kino/MovieDetails.cs
+++ Kino/MovieDetails.cs
@@ -21,9 +21,13 @@ namespace Kino
         DataTable dtActresses;
         DataTable dtDirector;
         DataTable dtWriter;
+        DataTable dtMovieId;
+        DataTable dtRating;
         OracleDataAdapter da;
         public string QueryMovieDetails { get; set; }
         public string QueryRoles { get; set; }
+        public int MovieId { get; set; }
+        public decimal Rating { get; set; }
 
         public MovieDetails(string MovieName, OracleConnection conn)
         {
@@ -39,6 +43,9 @@ namespace Kino
             dtActresses = new DataTable();
             dtDirector = new DataTable();
             dtWriter = new DataTable();
+            dtMovieId = new DataTable();
+            dtRating = new DataTable();
+
 
             da.SelectCommand = new OracleCommand(QueryMovieDetails, conn);
             da.Fill(dtMovieDetails);
@@ -54,7 +61,9 @@ namespace Kino
             da.Dispose();
             da.SelectCommand = new OracleCommand(QueryRoles + "'Writer'", conn);
             da.Fill(dtWriter);
-            
+
+            MovieId = GetMovieId();
+            Rating = CalculateRating();
             FillLabels();    
         }
 
@@ -64,7 +73,7 @@ namespace Kino
             tbDuration.Text = dtMovieDetails.Rows[0][2].ToString();
             tbYear.Text = dtMovieDetails.Rows[0][3].ToString();
             tbGenre.Text = dtMovieDetails.Rows[0][4].ToString();
-            tbRating.Text = dtMovieDetails.Rows[0][5].ToString();
+            tbRating.Text = Rating.ToString();
         
             foreach(DataRow dr in dtActors.Rows){
                 tbActors.Text += dr[0].ToString() + " " + dr[1].ToString() + "; ";
@@ -82,14 +91,15 @@ namespace Kino
                 tbWriter.Text += dr[0].ToString() + " " + dr[1].ToString() + "; ";
             }
         }
-        private void btnAddRating_Click(object sender, EventArgs e)
+        
+        /*private void btnAddRating_Click(object sender, EventArgs e)
         {
             if (tbAddRating.Text.Trim().Length > 0) {
                 int rating = 0;
                 int.TryParse(tbAddRating.Text, out rating);
                 //боље да нема оценување од страна на клиентите зашто требаше да биде листа од рејтинзи полето рејтинг некако во базата. Вака нема како да го рачунаме :S
             }
-        }
+        }*/
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -98,6 +108,95 @@ namespace Kino
         private void label1_MouseEnter(object sender, EventArgs e)
         {
 
+        }
+
+        public int GetMovieId() {
+            String Query = "SELECT MOVIEID FROM MOVIES WHERE MOVIENAME = '" + MovieName.Trim() + "'";
+            da.SelectCommand = new OracleCommand(Query, conn);
+            da.Fill(dtMovieId);
+            return Convert.ToInt16(dtMovieId.Rows[0][0].ToString().Trim());
+        }
+
+        public decimal CalculateRating() {
+            //presmetaj go prosekot od rejtinzite za filmot sekogas koga nekoj ke glasa
+            String Query = "SELECT CAST(AVG(RATING) AS DECIMAL (5, 2)) FROM RATINGS WHERE MOVIEID = " + MovieId;
+            da.SelectCommand = new OracleCommand(Query, conn);
+            dtRating.Dispose();
+            dtRating = new DataTable();
+            da.Fill(dtRating);
+            return Convert.ToDecimal(dtRating.Rows[0][0].ToString().Trim());
+        }
+
+        public void RateMovie(int rating) {
+            //vnesi podatoci za rating vo bazata
+            string NewBookingQuery = "INSERT INTO RATINGS VALUES(" + rating.ToString() + ", " + MovieId.ToString() + ", SYSDATE)";
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = NewBookingQuery;
+            int check = cmd.ExecuteNonQuery();
+           
+            //iskluci gi site radio buttons za da ne moze da glasa povtorno
+            DisableAllButtons();
+
+            Rating = CalculateRating();
+            tbRating.Text = Rating.ToString();
+        }
+
+        //kako gi pravam disable ovie so eden for znaci..
+        public void DisableAllButtons() {
+            for (int i = 1; i <= 10; i++) {
+                this.Controls.Find("radioButton" + i.ToString(), true)[0].Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(1);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(2);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(3);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(4);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(5);
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(6);
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(7);
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(8);
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(9);
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            RateMovie(10);
         }
     }
 }
